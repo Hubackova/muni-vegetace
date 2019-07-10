226231ee5c1ba272ef2c5a4c175ae12d2135ee86
Lenka <hubackova.lenka@gmail.com>
2019-07-10 19:24:30 +0200

    personbox update

--- src/components/people/PersonBox.js
+++ src/components/people/PersonBox.js
@@ -1,75 +1,63 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import researchgate from "../../../static/images/social-researchgate.png";
 import is from "../../../static/images/social-is.png";
 
-class PersonBox extends Component {
-  state = { showDetail: false };
 
-  toggleShowDetail = () => {
-    this.setState(prevState => ({
-      showDetail: !prevState.showDetail
-    }));
-  };
 
-  render() {
-    const { personInfo, data } = this.props;
-    const { showDetail } = this.state;
-    const hasDetail = personInfo.description.length > 0;
-    return (
-      <Fragment>
-        <Box>
-          <BoxPart style={{ flex: 1, paddingLeft: 0 }}>
-            <div style={{ paddingTop: 10 }}>
-              {personInfo.img ? (
-                <img src={personInfo.img} alt={personInfo.name} height="265px" />
-              ) : (
-                <div style={{ height: 265, width: 200, border: "1px solid black" }} />
+const PersonBox = ({ personInfo }) => {
+  return (
+    <Box>
+      <BoxPart style={{ flex: 1, paddingLeft: 0 }}>
+        <div style={{ paddingTop: 10 }}>
+          {personInfo.img ? (
+            <img src={personInfo.img} alt={personInfo.name} height="265px" />
+          ) : (
+            <div style={{ height: 265, width: 200, border: "1px solid black" }} />
+          )}
+        </div>
+      </BoxPart>
+      <BoxPart style={{ flex: 6, minWidth: 260 }}>
+        <Name>
+          {personInfo.name}
+          <div style={{ fontSize: "0.7em", color: "#778899" }}>{personInfo.position}</div>
+        </Name>
+        <Info>
+          <div>
+            <P>
+              <i className="fa fa-envelope fa-lg" style={{ color: "black", marginRight: 15 }} />
+              <span>{personInfo.email}</span>
+            </P>
+            {personInfo.phoneNumber && (
+              <P>
+                <i className="fa fa-phone fa-lg" style={{ marginRight: 15 }} />
+                {personInfo.phoneNumber}
+              </P>
+            )}
+            <P>
+              {personInfo.researchgate && (
+                <a target="_blank" rel="noopener noreferrer" href={personInfo.researchgate}>
+                  <img src={researchgate} alt="researchgate" height="40px" />
+                </a>
               )}
-            </div>
-          </BoxPart>
-          <BoxPart style={{ flex: 6, minWidth: 260 }}>
-            <Name>
-              {personInfo.name}
-              <div style={{ fontSize: "0.7em", color: "#778899" }}>{personInfo.position}</div>
-            </Name>
-            <Info>
-              <div>
-                <P>
-                  <i className="fa fa-envelope fa-lg" style={{ color: "black", marginRight: 15 }} />
-                  <span>{personInfo.email}</span>
-                </P>
-                {personInfo.phoneNumber && (
-                  <P>
-                    <i className="fa fa-phone fa-lg" style={{ marginRight: 15 }} />
-                    {personInfo.phoneNumber}
-                  </P>
-                )}
-                <P>
-                  {personInfo.researchgate && (
-                    <a target="_blank" rel="noopener noreferrer" href={personInfo.researchgate}>
-                      <img src={researchgate} alt="researchgate" height="40px" />
-                    </a>
-                  )}
-                  {personInfo.is && (
-                    <a target="_blank" rel="noopener noreferrer" href={personInfo.is}>
-                      <img src={is} alt="is" height="40px" />
-                    </a>
-                  )}
-                </P>
-              </div>
-           
-            <Description>
-              <div>{personInfo.description}</div>
-            </Description> </Info>
-          </BoxPart>
-        </Box>
-      </Fragment>
-    );
-  }
-}
+              {personInfo.is && (
+                <a target="_blank" rel="noopener noreferrer" href={personInfo.is}>
+                  <img src={is} alt="is" height="40px" />
+                </a>
+              )}
+            </P>
+          </div>
+          <Description>
+            <div>{personInfo.description}</div>
+          </Description>{" "}
+        </Info>
+      </BoxPart>
+    </Box>
+  );
+};
+
 export default PersonBox;
 
 PersonBox.propTypes = {
@@ -78,11 +66,14 @@ PersonBox.propTypes = {
 };
 
 const Box = styled.div`
+  font-size: 0.8em;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   padding: 1em;
+  margin-bottom: 1em;
   @media (max-width: ${props => props.theme.largeDevice}) {
+    flex-direction: column;
     padding: 0;
   }
 `;
@@ -91,12 +82,14 @@ const Name = styled.h3`
   color: ${props => props.theme.main};
   text-transform: uppercase;
   margin: 0;
-  margin-bottom: 20px;
   border-bottom: 0.08em solid rgb(229, 229, 229);
 `;
 
 const BoxPart = styled.div`
   padding: 0 1em;
+  @media (max-width: ${props => props.theme.largeDevice}) {
+    padding: 10px 0;
+  }
 `;
 
 const P = styled.p`
@@ -104,17 +97,17 @@ const P = styled.p`
 `;
 
 const Description = styled.div`
-   padding-left: 2em; 
-   flex: 4;
-   @media (max-width: ${props => props.theme.extraLargeDevice}) {
-    padding-left: 0; 
+  padding-left: 2em;
+  flex: 4;
+  @media (max-width: ${props => props.theme.extraLargeDevice}) {
+    padding-left: 0;
   }
 `;
 
 const Info = styled.div`
-   display: flex; 
-   flex-direction: row;
-   @media (max-width: ${props => props.theme.extraLargeDevice}) {
+  display: flex;
+  flex-direction: row;
+  @media (max-width: ${props => props.theme.extraLargeDevice}) {
     flex-direction: column;
   }
 `;
