a79ab411eb8e32847a6447cbd11f41ce7c7d0b10
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-07 14:27:49 +0200

    disertacky cesky

--- src/content/theses.js
+++ src/content/theses.js
@@ -25,7 +25,7 @@
 
   phdThesesList: [
     {
-      name: `Diversity and ecology of epiphytic bryophyte communities on dead wood in forest nature reserves.`,
+      name: `Diversity and ecology of epiphytic bryophyte communities on dead wood in forest nature reserves`,
       link: `https://is.muni.cz/auth/th/eqf1z/`,
       author: "Markéta Táborská",
       year: "2018"
@@ -67,18 +67,18 @@
       year: "2014"
     },
 	{
-      name: `Environmental control of diversity of Central European grasslands`,
-      link: `https://is.muni.cz/auth/th/gyqwk/`,
-      author: "Kristína Merunková",
-      year: "2013"
-    },
-	{
       name: `Diversity of mountain forest vegetation in Taiwan`,
       link: `https://is.muni.cz/auth/th/fhgaf/`,
       author: "Ching-Feng Li",
       year: "2013"
     },
 	{
+      name: `Environmental control of diversity of Central European grasslands`,
+      link: `https://is.muni.cz/auth/th/gyqwk/`,
+      author: "Kristína Merunková",
+      year: "2013"
+    },
+		{
       name: `The effect of productivity and other environmental factors on the forest herb-layer diversity`,
       link: `https://is.muni.cz/auth/th/g64sa/?lang=cs`,
       author: "Irena Axmanová",
@@ -86,10 +86,10 @@
     }
 	  ],
 
-  thesesTitle: "Theses topics (selection)",
-  bachelorTheses: "Bachelor Theses",
-  masterTheses: "Master Theses",
-  phdTheses: "PhD Theses"
+  thesesTitle: "Theses topics",
+  bachelorTheses: "Bachelor Theses (examples)",
+  masterTheses: "Master Theses (examples)",
+  phdTheses: "PhD Theses (since 2010)"
 };
 
 export const thesesCz = {
@@ -119,27 +119,69 @@ export const thesesCz = {
 
   phdThesesList: [
      {
-      name: `Diversity and ecology of epiphytic bryophyte communities on dead wood in forest nature reserves`,
+      name: `Diverzita a ekologie společenstev epifytických mechorostů na mrtvém dřevě v lesních rezervacích`,
       link: `https://is.muni.cz/auth/th/eqf1z/`,
       author: "Markéta Táborská",
       year: "2018"
     },
+	{
+      name: `Diverzita vegetace evropských měst`,
+      link: `https://is.muni.cz/auth/th/l2m0i/`,
+      author: "Natálie Čeplová",
+      year: "2017"
+    },
+	{
+      name: `Vliv faktorů prostředí na lokální druhové bohatství rostlin v eurasijských suchých trávnících: makroekologická perspektiva`,
+      link: `https://is.muni.cz/auth/th/pqmhh/`,
+      author: "Salza Palpurina",
+      year: "2017"
+    },
+	{
+      name: `Prostorové rozmístění maloplošné diverzity vegetace v krajině`,
+      link: `https://is.muni.cz/auth/th/qh9b3/?lang=cs`,
+      author: "Dana Holubová",
+      year: "2016"
+    },
 	 {
+      name: `Tradiční management a jeho vliv na vegetaci podrostu světlých lesů nížin`,
+      link: `https://is.muni.cz/auth/th/pyveg/`,
+      author: "Ondřej Vild",
+      year: "2016"
+    },
+	 {
+      name: `Invaze nepůvodních druhů rostlin v biotopech: mezikontinentální srovnání`,
+      link: `https://is.muni.cz/auth/th/iiosh/`,
+      author: "Veronika Kalusová",
+      year: "2015"
+    },
+	{
+      name: `Vliv fenologických změn vegetace na sběr a analýzu vegetačních dat`,
+      link: `https://is.muni.cz/auth/th/zgrjc/`,
+      author: "Marie Vymazalová",
+      year: "2014"
+    },
+	{
+      name: `Diverzita horské lesní vegetace na Tchaj-wanu`,
+      link: `https://is.muni.cz/auth/th/fhgaf/`,
+      author: "Ching-Feng Li",
+      year: "2013"
+    },
+	{
+      name: `Vliv prostředí na diverzitu středoevropské travinné vegetace`,
+      link: `https://is.muni.cz/auth/th/gyqwk/`,
+      author: "Kristína Merunková",
+      year: "2013"
+    },
+	{
       name: `Vliv produktivity a dalších faktorů prostředí na diverzitu bylinného patra lesů`,
       link: `https://is.muni.cz/auth/th/g64sa/?lang=cs`,
       author: "Irena Axmanová",
       year: "2012"
-    },
-	{
-      name: `Spatial distribution of small scale vegetation diversity in the landscape`,
-      link: `https://is.muni.cz/auth/th/qh9b3/?lang=cs`,
-      author: "Dana Holubová",
-      year: "2016"
     }
   ],
 
-  thesesTitle: "Témata prací (Výběr)",
-  bachelorTheses: "Balakářské práce",
-  masterTheses: "Magisterské práce",
-  phdTheses: "Disertační práce"
+  thesesTitle: "Témata prací",
+  bachelorTheses: "Balakářské práce (výběr)",
+  masterTheses: "Magisterské práce (výběr)",
+  phdTheses: "Disertační práce (po roce 2010)"
 };
