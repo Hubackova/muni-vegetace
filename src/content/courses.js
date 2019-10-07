e740af62eb979a232bee225edab863f6c8eba763
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-07 14:58:59 +0200

    další výuka

--- src/content/courses.js
+++ src/content/courses.js
@@ -4,12 +4,40 @@ export const coursesCz = [
     link: `http://botzool.sci.muni.cz/study/seminar_bot.pdf`
   },
   {
+    name: `Ekologie lesa`,
+    link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi8185`
+  },
+  {
     name: `Ekologie společenstev a makroekologie`,
 	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi6340`
   },
   {
     name: `Chráněná území střední Evropy`,
 	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi9980`
+  },
+  {
+    name: `Management ohrožených ekosystémů`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi7113`
+  },
+  {
+    name: `Populační ekologie rostlin`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi5210`
+  },
+  {
+    name: `Právo a státní správa v ochraně přírody`,
+    link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi7710`
+  },
+  {
+    name: `Příroda ve čtvrtohorách`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi8300`
+  },
+  {
+    name: `Terénní cvičení z geobotaniky`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6661`
+  },
+  {
+    name: `Terénní exkurze k environmentálním problémům`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi7111`
   },
   {
     name: `Úvod do studia ekologické a evoluční biologie`,
@@ -21,11 +49,15 @@ export const coursesCz = [
   },
   {
     name: `Vegetace a biotopy Evropy`,
-	link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6540`
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi9420`
   },
   {
     name: `Zahraniční botanická exkurze`,
     link: `http://botanicka-exkurze.cz`
+  },
+  {
+    name: `Zpracování základních botanických dat`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6549`
   },
   
 ];
