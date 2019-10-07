7816c2b9627dc6fcfc980551a0526ead6e153f8b
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-07 14:47:03 +0200

    výuka

--- src/content/courses.js
+++ src/content/courses.js
@@ -1,45 +1,63 @@
 export const coursesCz = [
   {
+    name: `Botanický seminář`,
+    link: `http://botzool.sci.muni.cz/study/seminar_bot.pdf`
+  },
+  {
     name: `Ekologie společenstev a makroekologie`,
-
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi6340`
   },
   {
-    name: `Vegetace České republiky`,
-    link: `https://is.muni.cz/predmet/sci/podzim2018/Bi8175`
+    name: `Chráněná území střední Evropy`,
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi9980`
   },
   {
-    name: `Vegetace Evropy`,
-
+    name: `Úvod do studia ekologické a evoluční biologie`,
+    link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi1000`
+  },
+  {
+    name: `Vegetace a biotopy ČR`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6540`
+  },
+  {
+    name: `Vegetace a biotopy Evropy`,
+	link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6540`
   },
   {
     name: `Zahraniční botanická exkurze`,
     link: `http://botanicka-exkurze.cz`
   },
-  {
-    name: `Botanický seminář`,
-    link: `http://botzool.sci.muni.cz/study/seminar_bot.pdf`
-  },
+  
 ];
 
 export const coursesEn = [
   {
+    name: `Botany seminar`,
+    link: `http://botzool.sci.muni.cz/study/seminar_bot.pdf`
+  },
+  {
     name: `Community ecology and macroecology`,
-
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi6340?lang=en`
   },
   {
-    name: `Vegetation of the Czech Republic`,
-    link: `https://is.muni.cz/predmet/sci/podzim2018/Bi8175`
+    name: `Protected areas of Central Europe`,
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi9980?lang=en`
   },
   {
-    name: `Vegetation of Europe`,
-
+    name: `Introduction to study of ecological and evolutionary biology`,
+    link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi1000?lang=en`
+  },
+  {
+    name: `Vegetation and habitats of the Czech Republic`,
+    link: `https://is.muni.cz/auth/predmet/sci/jaro2020/Bi6540?lang=en;setlang=en`
+  },
+  {
+    name: `Vegetation and habitats of Europe`,
+	link: `https://is.muni.cz/auth/predmet/sci/podzim2019/Bi9420?lang=en`
   },
   {
     name: `Botanical excursion abroad`,
     link: `http://botanicka-exkurze.cz`
   },
-  {
-    name: `Botany seminar`,
-    link: `http://botzool.sci.muni.cz/study/seminar_bot.pdf`
-  },
+  
 ];
