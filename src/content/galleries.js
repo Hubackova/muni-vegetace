a998065ca4f80b410ac1d63d5d1d31e81d4b24db
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-08 14:27:24 +0200

    galerie abroad

--- src/content/galleries.js
+++ src/content/galleries.js
@@ -1,19 +1,22 @@
 export const galleryNames = [
   { main: "excursions", subgalleries: ["trip2009", "zahranicni2018"] },
-  { main: "others" }
+  { main: "others" },
+  { main: "abroad", subgalleries: ["abroad2018"] }
 ];
 
-export const galleryNamesCz = ["Terenní exkurze", "Ostatní"];
-export const galleryNamesEn = ["Excursions", "Other Photos"];
+export const galleryNamesCz = ["Terenní exkurze", "Ostatní", "Zahraniční exkurze", "2018 Korsika"];
+export const galleryNamesEn = ["Excursions", "Other Photos", "Excursions abroad", "2018 Corsica"];
 
 export const galleryLabelsCz = {
   excursions: ["popisek k první fotce", "popisek k druhé fotce"],
   others: ["první fotka", "další popisek", "třeté popisek"],
   zahranicni2018: ["první fotka 2018"],
+  abroad2018: ["Korsika01"],
 };
 
 export const galleryLabelsEn = {
   excursions: ["first photo label", "another one"],
   others: ["first", "next", "3rd"],
   zahranicni2018: ["first photo 2018"],
+  abroad2018: ["Korsika01"],
 };
