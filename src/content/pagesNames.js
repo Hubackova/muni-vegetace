fae7f4f8f443933237ba44e9937eca96b7c636f4
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-08 17:35:51 +0200

    add mohelno a macedonia

--- src/content/pagesNames.js
+++ src/content/pagesNames.js
@@ -1,3 +1,3 @@
-exports.galleryNames = ["excursions", "others", "trip2009", "zahranicni2018", "abroad", "2018corse", "2017romania", "2016albania", "2015serbia", "2014poland", "2013sardinia", "2012swiss", "2011turkey"]
+exports.galleryNames = ["excursions", "others", "trip2009", "zahranicni2018", "2019mohelno" "abroad", "2019macedonia", "2018corsica", "2017romania", "2016albania", "2015serbia", "2014poland", "2013sardinia", "2012swiss", "2011turkey"]
 exports.projectsNames = ["project1", "project2", "project3", "project4", "project5"]
   
