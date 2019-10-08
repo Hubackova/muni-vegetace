d8dfcb34ef35b2386d3805143186d2b5bfd39165
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-08 17:44:36 +0200

    add mohelno a macedonia

--- src/content/galleries.js
+++ src/content/galleries.js
@@ -1,22 +1,24 @@
 export const galleryNames = [
-  { main: "excursions", subgalleries: ["trip2009", "zahranicni2018"] },
+  { main: "excursions", subgalleries: ["trip2009", "zahranicni2018", "2019mohelno"] },
   { main: "others" },
   { main: "abroad", subgalleries: ["2019macedonia", "2018corsica", "2017romania", "2016albania", "2015serbia", "2014poland", "2013sardinia", "2012swiss", "2011turkey" ] },
 ];
 
-export const galleryNamesCz = ["Terenní exkurze", "Ostatní", "Zahraniční exkurze", "2019 Makedonie", "2018 Korsika", "2017 Rumunsko", "2016 Albánie + Černá Hora", "2015 Srbsko", "2014 Polsko", "2013 Sardinie", "2012 Švýcarsko", "2011 Turecko"];
-export const galleryNamesEn = ["Excursions", "Other Photos", "Excursions abroad", "2019 Macedonia", "2018 Corsica", "2017 Romania", "2016 Albania + Montenegro", "2015 Serbia", "2014 Poland", "2013 Sardinia", "2012 Switzerland", "2011 Turkey"];
+export const galleryNamesCz = ["Terenní exkurze", "Ostatní", "2019 Mohelno", "Zahraniční exkurze", "2019 Makedonie", "2018 Korsika", "2017 Rumunsko", "2016 Albánie + Černá Hora", "2015 Srbsko", "2014 Polsko", "2013 Sardinie", "2012 Švýcarsko", "2011 Turecko"];
+export const galleryNamesEn = ["Excursions", "Other Photos", "2019 Mohelno", "Excursions abroad", "2019 Macedonia", "2018 Corsica", "2017 Romania", "2016 Albania + Montenegro", "2015 Serbia", "2014 Poland", "2013 Sardinia", "2012 Switzerland", "2011 Turkey"];
 
 export const galleryLabelsCz = {
   excursions: ["popisek k první fotce", "popisek k druhé fotce"],
   others: ["první fotka", "další popisek", "třeté popisek"],
   zahranicni2018: ["první fotka 2018"],
-  abroad2018: ["Korsika01"],
+  2018corsica: ["Korsika01"],
+  2019macedonia: ["Makedonie01"],
 };
 
 export const galleryLabelsEn = {
   excursions: ["first photo label", "another one"],
   others: ["first", "next", "3rd"],
   zahranicni2018: ["first photo 2018"],
-  abroad2018: ["Korsika01"],
+  2018corsica: ["Corsica01"],
+  2019macedonia: ["Macedonia01"],
 };
