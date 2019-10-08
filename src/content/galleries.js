cb7030978533aae683f1945728bb1a1f4a83dfc5
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-08 18:07:47 +0200

    pokus o napravu

--- src/content/galleries.js
+++ src/content/galleries.js
@@ -11,14 +11,10 @@ export const galleryLabelsCz = {
   excursions: ["popisek k první fotce", "popisek k druhé fotce"],
   others: ["první fotka", "další popisek", "třeté popisek"],
   zahranicni2018: ["první fotka 2018"],
-  2018corsica: ["Korsika01"],
-  2019macedonia: ["Makedonie01"],
-};
+  };
 
 export const galleryLabelsEn = {
   excursions: ["first photo label", "another one"],
   others: ["first", "next", "3rd"],
   zahranicni2018: ["first photo 2018"],
-  2018corsica: ["Corsica01"],
-  2019macedonia: ["Macedonia01"],
-};
+  };
