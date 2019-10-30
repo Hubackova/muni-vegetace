71d548c231566499960fb75bfcfbcdab839890be
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-30 16:59:13 +0100

    aktualizace theses

--- src/content/theses.js
+++ src/content/theses.js
@@ -2,37 +2,37 @@
   bachelorThesesList: [
     {
       name: `Structure of exposure-related forest-steppe in Central Europe`,
-      link: `https://is.muni.cz/auth/th/tq01u/?lang=en`,
+      link: `https://is.muni.cz/th/tq01u/?lang=en`,
       author: "Kryštof Chytrý",
       year: "2019"
     },
     {
       name: `Vegetation succession on disused railways in the eastern part of the Czech Republic`,
-      link: `https://is.muni.cz/auth/th/h0k5v/?lang=en`,
+      link: `https://is.muni.cz/th/h0k5v/?lang=en`,
       author: "Filip Kratoš",
       year: "2019"
     },
     {
       name: `Species richness of selected vegetation types: comparison of modelled and observed numbers of species`,
-      link: `https://is.muni.cz/auth/th/pefba/?lang=en`,
+      link: `https://is.muni.cz/th/pefba/?lang=en`,
       author: "Andrea Lobpreisová",
       year: "2019"
     },
     {
       name: `Distribution of Oxalis corniculata, O. dillenii and O. stricta in the Czech Republic as documented by herbarium specimens in some public collections`,
-      link: `https://is.muni.cz/auth/th/fqiqn/?lang=en`,
+      link: `https://is.muni.cz/th/fqiqn/?lang=en`,
       author: "Jan Dohnal",
       year: "2018"
     },
     {
       name: `Diversity and ecology of terrestrial cyanobacteria and algae in caves of the Protected Landscape Area Moravian Karst`,
-      link: `https://is.muni.cz/auth/th/hitm3/?lang=en`,
+      link: `https://is.muni.cz/th/hitm3/?lang=en`,
       author: "Zuzana Hůlková",
       year: "2018"
     },
     {
       name: `Natural conditions and flora of mining sites of White Carpathians Mts.`,
-      link: `https://is.muni.cz/auth/th/rjhl3/?lang=en`,
+      link: `https://is.muni.cz/th/rjhl3/?lang=en`,
       author: "Eliška Kundratová",
       year: "2017"
     }
@@ -41,31 +41,31 @@
   masterThesesList: [
     {
       name: `Ecology and diversity of soil diatoms (Bacillariophyta) of Ile Amsterdam and Ile Saint-Paul (TAAF, Southern Indian Ocean)`,
-      link: `https://is.muni.cz/auth/th/b97qq/?lang=en`,
+      link: `https://is.muni.cz/th/b97qq/?lang=en`,
       author: "Tereza Cahová",
       year: "2019"
     },
     {
       name: `Alpine vegetation of Mount Smolikas (Greece) and Mount Nemërçkë (Albania)`,
-      link: `https://is.muni.cz/auth/th/nwy2q/?lang=en`,
+      link: `https://is.muni.cz/th/nwy2q/?lang=en`,
       author: "Mário Duchoň",
       year: "2018"
     },
     {
       name: `Flora and vegetation of the Bílá voda catchment in the Drahanská vrchovina Mountains`,
-      link: `https://is.muni.cz/auth/th/yefy1/?lang=en`,
+      link: `https://is.muni.cz/th/yefy1/?lang=en`,
       author: "Pavla Vlčková",
       year: "2018"
     },
     {
       name: `Local species richness of natural vegetation in the deep river valleys of the Bohemian Massif`,
-      link: `https://is.muni.cz/auth/th/oeak6/?lang=en`,
+      link: `https://is.muni.cz/th/oeak6/?lang=en`,
       author: "Jaroslav Hruban",
       year: "2017"
     },
     {
       name: `Flora of selected area on western margin of Brno`,
-      link: `https://is.muni.cz/auth/th/iu4t7/`,
+      link: `https://is.muni.cz/th/iu4t7/`,
       author: "Martina Staviařová",
       year: "2017"
     }
@@ -74,61 +74,61 @@
   phdThesesList: [
     {
       name: `Diversity and ecology of epiphytic bryophyte communities on dead wood in forest nature reserves`,
-      link: `https://is.muni.cz/auth/th/eqf1z/`,
+      link: `https://is.muni.cz/th/eqf1z/`,
       author: "Markéta Táborská",
       year: "2018"
     },
     {
       name: `Diversity of European urban vegetation`,
-      link: `https://is.muni.cz/auth/th/l2m0i/`,
+      link: `https://is.muni.cz/th/l2m0i/`,
       author: "Natálie Čeplová",
       year: "2017"
     },
     {
       name: `Environmental drivers of fine-scale plant species richness in Eurasian dry grasslands: a macroecological perspective`,
-      link: `https://is.muni.cz/auth/th/pqmhh/`,
+      link: `https://is.muni.cz/th/pqmhh/`,
       author: "Salza Palpurina",
       year: "2017"
     },
     {
       name: `Spatial distribution of small scale vegetation diversity in the landscape`,
-      link: `https://is.muni.cz/auth/th/qh9b3/?lang=cs`,
+      link: `https://is.muni.cz/th/qh9b3/?lang=cs`,
       author: "Dana Holubová",
       year: "2016"
     },
     {
       name: `Traditional management and its impact on the vegetation of open lowland woodlands`,
-      link: `https://is.muni.cz/auth/th/pyveg/`,
+      link: `https://is.muni.cz/th/pyveg/`,
       author: "Ondřej Vild",
       year: "2016"
     },
     {
       name: `Alien plant invasions in habitats: intercontinental comparison`,
-      link: `https://is.muni.cz/auth/th/iiosh/`,
+      link: `https://is.muni.cz/th/iiosh/`,
       author: "Veronika Kalusová",
       year: "2015"
     },
     {
       name: `The effect of phenological changes on vegetation data sampling and analyses`,
-      link: `https://is.muni.cz/auth/th/zgrjc/`,
+      link: `https://is.muni.cz/th/zgrjc/`,
       author: "Marie Vymazalová",
       year: "2014"
     },
     {
       name: `Diversity of mountain forest vegetation in Taiwan`,
-      link: `https://is.muni.cz/auth/th/fhgaf/`,
+      link: `https://is.muni.cz/th/fhgaf/`,
       author: "Ching-Feng Li",
       year: "2013"
     },
     {
       name: `Environmental control of diversity of Central European grasslands`,
-      link: `https://is.muni.cz/auth/th/gyqwk/`,
+      link: `https://is.muni.cz/th/gyqwk/`,
       author: "Kristína Merunková",
       year: "2013"
     },
     {
       name: `The effect of productivity and other environmental factors on the forest herb-layer diversity`,
-      link: `https://is.muni.cz/auth/th/g64sa/?lang=cs`,
+      link: `https://is.muni.cz/th/g64sa/?lang=cs`,
       author: "Irena Axmanová",
       year: "2012"
     }
@@ -144,37 +144,37 @@ export const thesesCz = {
   bachelorThesesList: [
     {
       name: `Struktura expoziční lesostepi ve střední Evropě`,
-      link: `https://is.muni.cz/auth/th/tq01u/`,
+      link: `https://is.muni.cz/th/tq01u/`,
       author: "Kryštof Chytrý",
       year: "2019"
     },
     {
       name: `Sukcese vegetace na zrušených železničních tratích ve východní části České republiky`,
-      link: `https://is.muni.cz/auth/th/h0k5v/?lang=cs`,
+      link: `https://is.muni.cz/th/h0k5v/?lang=cs`,
       author: "Filip Kratoš",
       year: "2019"
     },
     {
       name: `Druhová bohatost vybraných vegetačních typů: srovnání modelovaného a pozorovaného počtu druhů`,
-      link: `https://is.muni.cz/auth/th/pefba/?lang=cs`,
+      link: `https://is.muni.cz/th/pefba/?lang=cs`,
       author: "Andrea Lobpreisová",
       year: "2019"
     },
     {
       name: `Rozšíření šťavelů Oxalis corniculata, O. dillenii a O. stricta v České republice podle herbářových dokladů ve vybraných veřejných sbírkách`,
-      link: `https://is.muni.cz/auth/th/fqiqn/`,
+      link: `https://is.muni.cz/th/fqiqn/`,
       author: "Jan Dohnal",
       year: "2018"
     },
     {
       name: `Diverzita a ekologie terestrických sinic a řas v jeskyních CHKO Moravský kras`,
-      link: `https://is.muni.cz/auth/th/hitm3/`,
+      link: `https://is.muni.cz/th/hitm3/`,
       author: "Zuzana Hůlková",
       year: "2018"
     },
     {
       name: `Přírodní poměry a flóra lomů v Bílých Karpatech`,
-      link: `https://is.muni.cz/auth/th/rjhl3/`,
+      link: `https://is.muni.cz/th/rjhl3/`,
       author: "Eliška Kundratová",
       year: "2017"
     }
@@ -183,31 +183,31 @@ export const thesesCz = {
   masterThesesList: [
     {
       name: `Ekologie a diverzita půdních rozsivek ostrova Nový Amsterdam a ostrova Svatého Pavla (TAAF, Jižní Indický oceán)`,
-      link: `https://is.muni.cz/auth/th/b97qq/`,
+      link: `https://is.muni.cz/th/b97qq/`,
       author: "Tereza Cahová",
       year: "2019"
     },
     {
       name: `Alpínská vegetace pohoří Smolikas v Řecku a Nemërçkë v Albánii`,
-      link: `https://is.muni.cz/auth/th/nwy2q/`,
+      link: `https://is.muni.cz/th/nwy2q/`,
       author: "Mário Duchoň",
       year: "2018"
     },
     {
       name: `Flóra a vegetace povodí Bílé vody na Drahanské vrchovině`,
-      link: `https://is.muni.cz/auth/th/yefy1/?lang=cs`,
+      link: `https://is.muni.cz/th/yefy1/?lang=cs`,
       author: "Pavla Vlčková",
       year: "2018"
     },
     {
       name: `Lokální druhové bohatství přirozené vegetace hlubokých říčních údolí Českého masivu`,
-      link: `https://is.muni.cz/auth/th/oeak6/?lang=cs`,
+      link: `https://is.muni.cz/th/oeak6/?lang=cs`,
       author: "Jaroslav Hruban",
       year: "2017"
     },
     {
       name: `Flóra vybraného územi na západním okraji Brna`,
-      link: `https://is.muni.cz/auth/th/iu4t7/`,
+      link: `https://is.muni.cz/th/iu4t7/`,
       author: "Martina Staviařová",
       year: "2017"
     }
@@ -216,61 +216,61 @@ export const thesesCz = {
   phdThesesList: [
     {
       name: `Diverzita a ekologie společenstev epifytických mechorostů na mrtvém dřevě v lesních rezervacích`,
-      link: `https://is.muni.cz/auth/th/eqf1z/`,
+      link: `https://is.muni.cz/th/eqf1z/`,
       author: "Markéta Táborská",
       year: "2018"
     },
     {
       name: `Diverzita vegetace evropských měst`,
-      link: `https://is.muni.cz/auth/th/l2m0i/`,
+      link: `https://is.muni.cz/th/l2m0i/`,
       author: "Natálie Čeplová",
       year: "2017"
     },
     {
       name: `Vliv faktorů prostředí na lokální druhové bohatství rostlin v eurasijských suchých trávnících: makroekologická perspektiva`,
-      link: `https://is.muni.cz/auth/th/pqmhh/`,
+      link: `https://is.muni.cz/th/pqmhh/`,
       author: "Salza Palpurina",
       year: "2017"
     },
     {
       name: `Prostorové rozmístění maloplošné diverzity vegetace v krajině`,
-      link: `https://is.muni.cz/auth/th/qh9b3/?lang=cs`,
+      link: `https://is.muni.cz/th/qh9b3/?lang=cs`,
       author: "Dana Holubová",
       year: "2016"
     },
     {
       name: `Tradiční management a jeho vliv na vegetaci podrostu světlých lesů nížin`,
-      link: `https://is.muni.cz/auth/th/pyveg/`,
+      link: `https://is.muni.cz/th/pyveg/`,
       author: "Ondřej Vild",
       year: "2016"
     },
     {
       name: `Invaze nepůvodních druhů rostlin v biotopech: mezikontinentální srovnání`,
-      link: `https://is.muni.cz/auth/th/iiosh/`,
+      link: `https://is.muni.cz/th/iiosh/`,
       author: "Veronika Kalusová",
       year: "2015"
     },
     {
       name: `Vliv fenologických změn vegetace na sběr a analýzu vegetačních dat`,
-      link: `https://is.muni.cz/auth/th/zgrjc/`,
+      link: `https://is.muni.cz/th/zgrjc/`,
       author: "Marie Vymazalová",
       year: "2014"
     },
     {
       name: `Diverzita horské lesní vegetace na Tchaj-wanu`,
-      link: `https://is.muni.cz/auth/th/fhgaf/`,
+      link: `https://is.muni.cz/th/fhgaf/`,
       author: "Ching-Feng Li",
       year: "2013"
     },
     {
       name: `Vliv prostředí na diverzitu středoevropské travinné vegetace`,
-      link: `https://is.muni.cz/auth/th/gyqwk/`,
+      link: `https://is.muni.cz/th/gyqwk/`,
       author: "Kristína Merunková",
       year: "2013"
     },
     {
       name: `Vliv produktivity a dalších faktorů prostředí na diverzitu bylinného patra lesů`,
-      link: `https://is.muni.cz/auth/th/g64sa/?lang=cs`,
+      link: `https://is.muni.cz/th/g64sa/?lang=cs`,
       author: "Irena Axmanová",
       year: "2012"
     }
