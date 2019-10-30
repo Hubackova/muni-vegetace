5154d09a1faa519d9a17335f63e6a56fd9350c31
Evka <Evka@hettenbe.sci.muni.cz>
2019-10-30 17:19:04 +0100

    add odkazy

--- src/content/links.js
+++ src/content/links.js
@@ -1,16 +1,64 @@
 ﻿/* eslint-disable no-irregular-whitespace */
 export const mediaCz = [
   {
-    date: `07/11/2018`,
+    date: `7/11/2018`,
     name: `Brněnští vědci odhalili tajemství úspěchu invazivních rostlin`,
     resource: `ČT24, ScienceMag, Online.muni.cz, Pozitivní zprávy`,
     link: `https://pozitivni-zpravy.cz/cesti-vedci-rozklicovali-tajemstvi-uspechu-invazivnich-rostlin/`
   },
   {
-    date: `01/04/2018`,
+    date: `1/4/2018`,
     name: `Každý druh má v ekosystému svou roli`,
     resource: `Online.muni.cz`,
     link: `https://www.em.muni.cz/veda-a-vyzkum/10378-kazdy-druh-ma-v-ekosystemu-svou-roli`
+  },
+  {
+    date: `24/3/2018`,
+    name: `Česká flóra a vegetace na jednom místě`,
+    resource: `Česká televize, Studio 6`,
+    link: `https://www.ceskatelevize.cz/ivysilani/10441287766-studio-6-vikend/218411010120324/obsah/609284-ceska-flora-a-vegetace-na-jednom-miste`
+  },
+  {
+    date: `20/3/2018`,
+    name: `Botanici spustili nejrozsáhlejší online databázi české flóry a vegetace`,
+    resource: `Česká televize, České noviny, MUNI, Radio.cz, Sciencemag, Ekolist, Enviweb, 21. století, Týden.cz`,
+    link: ``
+  },
+  {
+    date: `11/3/2018`,
+    name: `Evropské druhy rostlin obsadily svět díky soužití s člověkem`,
+    resource: `MUNI`,
+    link: `https://www.em.muni.cz/veda-a-vyzkum/10308-evropske-druhy-rostlin-obsadily-svet-diky-souziti-s-clovekem`
+  },
+  {
+    date: `9/1/2018`,
+    name: `Evropské rostliny ve světě`,
+    resource: `Česká televize (1:11:00)`,
+    link: `https://www.ceskatelevize.cz/ivysilani/10101491767-studio-ct24/218411058060109`
+  },
+  {
+    date: `9/3/2017`,
+    name: `Červený seznam ohrožených ekosystémů`,
+    resource: `Česká televize, Studio 6`,
+    link: `https://www.ceskatelevize.cz/ivysilani/1096902795-studio-6/217411010100309/obsah/528996-seznam-ohrozenych-ekosystemu-v-evrope`
+  },
+   {
+    date: `19/1/2017`,
+    name: `První evropský červený seznam biotopů`,
+    resource: `Online.muni.cz, Ekolist.cz, Týden.cz, Novinky.cz (20/1), Lidovky.cz (29/1), Brněnský Deník (8/2)`,
+    link: ``
+  },
+   {
+    date: `6/12/2016`,
+    name: `První úplný přehled typů evropské vegetace`,
+    resource: `Online.muni.cz, Movinky.cz (7/12)`,
+    link: ``
+  },
+   {
+    date: `3/1/2013`,
+    name: `Aktuální soupis naší flóry (Jiří Danihelka)`,
+    resource: `Český rozhlas Leonardo, Natura`,
+    link: `https://podtail.nl/podcast/natura-1/aktualni-soupis-na-i-flory-ji-i-danihelka-mok-ady-/`
   }
 ];
 
