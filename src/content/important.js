5edef49f5fbf4af645e02724c53d916130f58d4f
Lenka <hubackova.lenka@gmail.com>
2020-01-02 20:00:36 +0100

    homepage links

---
+++ src/content/important.js
@@ -0,0 +1,66 @@
+
+export const en = {
+  resourcesList: [
+    {
+      name: "European Vegetation Syntheses",
+      linkTo: "/project1"
+    },
+    {
+      name: "Biodiversity of European cities",
+      linkTo: "/project3"
+    },
+    {
+      name: "Česká národní fytocenologická databáze",
+      linkTo: "/phytosociologicalDb",
+      img: "resources-cnfd_en"
+    },
+    {
+      name: "JUICE",
+      linkTo: "/juice",
+      img: "resources-juice"
+    },
+    {
+      name: "Pladias",
+      linkTo: "/pladias",
+      img: "resources-pladias"
+    },
+    {
+      name: "European Vegetation Archive",
+      linkTo: "/eva",
+      img: "resources-eva"
+    }
+  ]
+};
+
+export const cz = {
+  resourcesList: [
+    {
+      name: "European Vegetation Syntheses",
+      linkTo: "/project1"
+    },
+    {
+      name: "Biodiversity of European cities",
+      linkTo: "/project3"
+    },
+    {
+      name: "Česká národní fytocenologická databáze",
+      linkTo: "/phytosociologicalDb",
+      img: "resources-cnfd_cz"
+    },
+    {
+      name: "JUICE",
+      linkTo: "/juice",
+      img: "resources-juice"
+    },
+    {
+      name: "Pladias",
+      linkTo: "/pladias",
+      img: "resources-pladias"
+    },
+    {
+      name: "European Vegetation Archive",
+      linkTo: "/eva",
+      img: "resources-eva"
+    }
+  ]
+};
