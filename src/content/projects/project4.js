330e829d14dfa7e7005cfe088e85d35c01b05404
Evka <Evka@hettenbe.sci.muni.cz>
2020-01-07 11:26:19 +0100

    add project 4 (fylogeneze)

--- src/content/projects/project4.js
+++ src/content/projects/project4.js
@@ -2,12 +2,114 @@ import React from "react";
 
 export const project4en = {
   name: "project4",
-  title: `Fylogenetická a funkční diverzita rostlinných společenstev`,
-  body: <div></div>
+  title: `Decoupled phylogenetic and functional diversity in European plant communities`,
+  body: (
+  <div>
+	<div>
+	  <h3>Coordination</h3> Zdeňka Lososová
+    </div>
+	<div>
+        <h3>Participants and collaborators</h3>
+        Natálie Čeplová, Jan Divíšek, Lars Götzenberger, Milan Chytrý, Veronika Kalusová, Jakub Těšitel, Martin Večeřa
+      </div>
+	  <div>
+        <h3>Funding</h3>
+        <li>2018–2020 Czech Science Foundation, project 18-02773S</li>
+      </div>
+    <div>
+        <h3>Project Aims</h3>
+        <div>
+          We aim to identify current centres of phylogenetic (PD) and functional diversity (FD) in Europe and to provide insights into ecological and evolutionary processes that shape plant community structure. Our specific aims are: 
+        </div>
+		<li>To identify communities in which PD and FD are decoupled.</li>
+        <li>To detect effect of novel species interactions on decoupled PD, FD or both parts of diversity of European plant communities.</li>
+		</div>
+	<div className="project-publication">
+        <h3>Publications</h3>
+        <h4>Research papers</h4>
+        <li>
+          Čeplová N., Lososová Z., Zelený D., Chytrý M., Danihelka J., Fajmon K., Láníková D.,
+          Preislerová Z., Řehořek V. &amp; Tichý L. (2015): Phylogenetic diversity of central-
+          European urban plant communities: effects of alien species and habitat types. Preslia 87:
+          1–16.<a href="http://www.sci.muni.cz/botany/chytry/Ceplova_etal2015_Preslia.pdf">fulltext in PDF</a>
+        </li>
+		<li>
+          de Bello F., Šmilauer P., Diniz-Filho J.A.F., Carmona C.P., Lososová Z., Herben T. & Götzenberger L.(2017): Decoupling phylogenetic and functional diversity to reveal hidden signals in community assembly. Methods in Ecology and Evolution 8: 1200-1211.
+        </li>
+		<li>
+           Divíšek J., Chytrý M. Beckage B., Gotelli N.J., Lososová Z., Pyšek P., Richardson D.M. & Molofsky J.(2018): Similarity of introduced plant species to native ones facilitates naturalization, but differences enhance invasion success. Nature Communications 9: 4631. - <a href="https://www.nature.com/articles/s41467-018-06995-4.pdf">fulltext in pdf</a>
+        </li>
+		<li>
+          Loiola P.P., de Bello F., Chytrý M., Götzenberger L., Carmona C.P., Pyšek P. & Lososová Z. (2018): Invaders among locals: Alien species decrease phylogenetic and functional diversity while increasing dissimilarity among native community members. Journal of Ecology 106: 2230-2241. - <a href="http://www.ibot.cas.cz/personal/pysek/pdf/Loiola et al_Invaders among locals_JEcol 2018(online early).pdf">fulltext in pdf</a>
+        </li>
+		<li>
+          Lososová Z., Čeplová N., Chytrý M., Tichý L., Danihelka J., Fajmon K., Láníková D., Preislerová Z. & Řehořek V. (2016): Is phylogenetic diversity a good proxy for functional diversity of plant communities? A case study from urban habitats. Journal of Vegetation Science 27: 1036-1046.- <a href="http://onlinelibrary.wiley.com/doi/10.1111/jvs.12414/epdf">fulltext in Wiley Online Library</a>
+        </li>
+		<li>
+         Lososová Z., Šmarda P., Chytrý M., Purschke O., Pyšek P., Sádlo J., Tichý L. & Winter M. (2015): Phylogenetic structure of plant species pools reflects habitat age on the geological time scale. Journal of Vegetation Science 26: 1080-1089. - <a href="http://onlinelibrary.wiley.com/doi/10.1111/jvs.12308/epdf">fulltext in Wiley Online Library</a>
+        </li>
+		<h4>Popularizations</h4>
+        <li>
+          Lososová Z., Chytrý M. & Pyšek P. (2018): Známe odpověď na Darwinovu naturalizační hádanku? Živa 5: 229-230. - <a href="http://www.ziva.avcr.cz/files/ziva/pdf/zname-odpoved-na-darwinovu-naturalizacni-hadanku.pdf">fulltext in pdf</a>
+        </li>
+	</div>
+  </div>
+  )
 };
 
 export const project4cz = {
   name: "project4",
   title: `Fylogenetická a funkční diverzita rostlinných společenstev`,
-  body: <div></div>
+  body: (
+  <div>
+	<div>
+	  <h3>Koordinátor</h3> Zdeňka Lososová
+    </div>
+	<div>
+        <h3>Spolupracovníci</h3>
+        Natálie Čeplová, Jan Divíšek, Lars Götzenberger, Milan Chytrý, Veronika Kalusová, Jakub Těšitel, Martin Večeřa
+      </div>
+	  <div>
+        <h3>Finanční podpora</h3>
+        <li>2018–2020 Czech Science Foundation, project 18-02773S</li>
+      </div>
+    <div>
+        <h3>Project Aims</h3>
+        <div>
+          We aim to identify current centres of phylogenetic (PD) and functional diversity (FD) in Europe and to provide insights into ecological and evolutionary processes that shape plant community structure. Our specific aims are: 
+        </div>
+		<li>To identify communities in which PD and FD are decoupled.</li>
+        <li>To detect effect of novel species interactions on decoupled PD, FD or both parts of diversity of European plant communities.</li>
+		</div>
+	<div className="project-publication">
+        <h3>Publikace</h3>
+        <h4>Odborné články</h4>
+        <li>
+          Čeplová N., Lososová Z., Zelený D., Chytrý M., Danihelka J., Fajmon K., Láníková D.,
+          Preislerová Z., Řehořek V. &amp; Tichý L. (2015): Phylogenetic diversity of central-
+          European urban plant communities: effects of alien species and habitat types. Preslia 87:
+          1–16.<a href="http://www.sci.muni.cz/botany/chytry/Ceplova_etal2015_Preslia.pdf">fulltext in PDF</a>
+        </li>
+		<li>
+          de Bello F., Šmilauer P., Diniz-Filho J.A.F., Carmona C.P., Lososová Z., Herben T. & Götzenberger L.(2017): Decoupling phylogenetic and functional diversity to reveal hidden signals in community assembly. Methods in Ecology and Evolution 8: 1200-1211.
+        </li>
+		<li>
+           Divíšek J., Chytrý M. Beckage B., Gotelli N.J., Lososová Z., Pyšek P., Richardson D.M. & Molofsky J.(2018): Similarity of introduced plant species to native ones facilitates naturalization, but differences enhance invasion success. Nature Communications 9: 4631. - <a href="https://www.nature.com/articles/s41467-018-06995-4.pdf">fulltext in pdf</a>
+        </li>
+		<li>
+          Loiola P.P., de Bello F., Chytrý M., Götzenberger L., Carmona C.P., Pyšek P. & Lososová Z. (2018): Invaders among locals: Alien species decrease phylogenetic and functional diversity while increasing dissimilarity among native community members. Journal of Ecology 106: 2230-2241. - <a href="http://www.ibot.cas.cz/personal/pysek/pdf/Loiola et al_Invaders among locals_JEcol 2018(online early).pdf">fulltext in pdf</a>
+        </li>
+		<li>
+          Lososová Z., Čeplová N., Chytrý M., Tichý L., Danihelka J., Fajmon K., Láníková D., Preislerová Z. & Řehořek V. (2016): Is phylogenetic diversity a good proxy for functional diversity of plant communities? A case study from urban habitats. Journal of Vegetation Science 27: 1036-1046.- <a href="http://onlinelibrary.wiley.com/doi/10.1111/jvs.12414/epdf">fulltext in Wiley Online Library</a>
+        </li>
+		<li>
+         Lososová Z., Šmarda P., Chytrý M., Purschke O., Pyšek P., Sádlo J., Tichý L. & Winter M. (2015): Phylogenetic structure of plant species pools reflects habitat age on the geological time scale. Journal of Vegetation Science 26: 1080-1089. - <a href="http://onlinelibrary.wiley.com/doi/10.1111/jvs.12308/epdf">fulltext in Wiley Online Library</a>
+        </li>
+		<h4>popularizační články</h4>
+        <li>
+          Lososová Z., Chytrý M. & Pyšek P. (2018): Známe odpověď na Darwinovu naturalizační hádanku? Živa 5: 229-230. - <a href="http://www.ziva.avcr.cz/files/ziva/pdf/zname-odpoved-na-darwinovu-naturalizacni-hadanku.pdf">fulltext in pdf</a>
+        </li>
+	</div>
+  </div>
+  )
 };
