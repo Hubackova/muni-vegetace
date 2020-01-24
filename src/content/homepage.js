e324a42dac0c4c5274af47e7ad0f102be5e50703
Evka <Evka@hettenbe.sci.muni.cz>
2020-01-24 16:11:42 +0100

    drobne zmeny 3

--- src/content/homepage.js
+++ src/content/homepage.js
@@ -2,7 +2,8 @@ import React, { Fragment } from "react";
 
 export const homepageEn = (
   <Fragment>
-    <h3>What do we do?</h3>
+   <h3>Vegetation Science Group</h3>
+   <h4>What do we do?</h4>
         <p align="justify">We are interested in the diversity of plant communities and its underlying factors. We investigate the distribution patterns of vegetation types and habitats in the landscape. We analyze in detail the species composition of different types of plant communities and ask how these communities originated, how they change due to natural and human influences and to what extent they are endangered. We deal not only with contemporary vegetation but also with changes in vegetation since the last Ice Age. Our area of interest is all of Europe and various areas of extratropical Asia. We perform basic vegetation surveys in the field as well as statistical analyses of large vegetation-plot databases.</p>
 <p align="justify">Our research is of high practical importance for nature conservation since we define vegetation and habitat types used for setting conservation priorities, inventorying, monitoring, planning appropriate management and defining targets of ecological restoration. We contribute to understanding how nature is affected by recent changes in land use, management, water regime, climate warming, eutrophication and the invasions of alien species, and we seek ways to reduce their negative impacts.</p>
           </Fragment>
@@ -10,7 +11,8 @@ export const homepageEn = (
 
 export const homepageCz = (
   <Fragment>
-    <h3>Čím se zabýváme?</h3>
+   <h3>Pracovní skupina Výzkum vegetace</h3>
+    <h4>Čím se zabýváme?</h4>
          <p align="justify">Zajímá nás diverzita rostlinných společenstev a faktory, které ji ovlivňují. Zkoumáme zákonitosti rozmístění vegetačních typů a biotopů v krajině. Podrobně analyzujeme druhové složení různých typů rostlinných společenstev a ptáme se, jak tato společenstva vznikla, jak se mění vlivem přírodních i lidských vlivů a do jaké míry jsou ohrožená. Zabýváme se nejen současnou vegetací, ale i změnami vegetace od poslední doby ledové. Naší zájmovou oblastí je celá Evropa a různé oblasti mimotropické Asie. Provádíme základní průzkum vegetace v terénu i statistické analýzy rozsáhlých vegetačních databází.</p>
 <p align="justify">Náš výzkum má význam pro ochranu přírody, protože definujeme typy vegetace a biotopů používaných v praxi pro stanovení ochranářských priorit, inventarizaci, monitoring, navrhování managementu a cílů ekologické obnovy. Přispíváme k poznání, jak přírodu ovlivňují současné změny využití krajiny, obhospodařování pozemků, vodního režimu, oteplování klimatu, eutrofizace a šíření nepůvodních druhů, a hledáme cesty, jak omezit jejich negativní dopady.
 </p>
