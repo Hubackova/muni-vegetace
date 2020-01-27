7b4ede660f7e21bf3d4732f65cc0287c92a02f95
Evka <Evka@hettenbe.sci.muni.cz>
2020-01-27 18:31:28 +0100

    drobné změny v resourc

--- src/content/links.js
+++ src/content/links.js
@@ -1,6 +1,12 @@
 ﻿/* eslint-disable no-irregular-whitespace */
 export const mediaCz = [
 {
+    date: `3/12/2019`,
+    name: `Botanici připravili nový klíč k určování rostlin. Minulý rychle zastaral`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/12300-botanici-pripravili-novy-klic-k-urcovani-rostlin?utm_source=fb_em&utm_medium=link&utm_campaign=facebook&fbclid=IwAR1--TWGjN1AddSnfvrM0w0geju_Yv_mkdOGzkKbLuhH3uMSn2yECESz45A`]
+  },
+  {
     date: `22/8/2019`,
     name: `Botanici zmapovali druhové bohatství evropských lesů`,
     resources: [`ČT Události v regionech`,`ČT24`, `Magazín M`],
@@ -19,6 +25,12 @@ export const mediaCz = [
     links: [`https://pozitivni-zpravy.cz/cesti-vedci-rozklicovali-tajemstvi-uspechu-invazivnich-rostlin/`,`https://ct24.ceskatelevize.cz/veda/2644739-brnensti-vedci-odhalili-tajemstvi-uspechu-invazivnich-rostlin-hlavni-roli-podle-nich`, `https://sciencemag.cz/invazni-rostliny-jsou-v-prumeru-vyssi-nez-puvodni-druhy/`, `https://www.online.muni.cz/veda-a-vyzkum/11178-objev-invazni-rostliny-jsou-v-prumeru-vyssi-nez-puvodni-druhy`]
   },
   {
+    date: `1/4/2018`,
+    name: `Každý druh má v ekosystému svou roli`,
+    resources: [`Online.muni.cz`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/10378-kazdy-druh-ma-v-ekosystemu-svou-roli`]
+  },
+  {
     date: `24/3/2018`,
     name: `Česká flóra a vegetace na jednom místě`,
     resources: [`Česká televize, Studio 6`],
@@ -35,6 +47,13 @@ export const mediaCz = [
 	`http://sciencemag.cz/nejrozsahlejsi-online-databaze-ceske-flory-a-vegetace/`, `https://ekolist.cz/cz/zpravodajstvi/zpravy/ode-dneska-je-pristupna-nejrozsahlejsi-online-databaze-ceske-flory-a-vegetace`,
 	`http://www.enviweb.cz/rss/164353`, `https://21stoleti.cz/2018/03/21/veskere-informace-o-ceske-kvetene-na-jednom-miste/`, `https://www.tyden.cz/rubriky/veda/priroda/webova-databaze-shrnuje-informace-o-flore-a-vegetaci-v-cesku_472193.html`, ]
   },
+  {
+    date: `11/3/2018`,
+    name: `Evropské druhy rostlin obsadily svět díky soužití s člověkem`,
+    resources: [`MUNI`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/10308-evropske-druhy-rostlin-obsadily-svet-diky-souziti-s-clovekem`]
+  },
+  
     {
     date: `9/1/2018`,
     name: `Evropské rostliny ve světě`,
@@ -48,42 +67,12 @@ export const mediaCz = [
     links: [`https://www.ceskatelevize.cz/ivysilani/1096902795-studio-6/217411010100309/obsah/528996-seznam-ohrozenych-ekosystemu-v-evrope`]
   },
   {
-    date: `3/1/2013`,
-    name: `Aktuální soupis naší flóry (Jiří Danihelka)`,
-    resources: [`Český rozhlas Leonardo`, `Natura`],
-    links: [`https://hledani.rozhlas.cz/iRadio/?query=danihelka&reader=&stanice%5B%5D=%C4%8CRo+Leonardo&porad%5B%5D=`, `https://podtail.nl/podcast/natura-1/aktualni-soupis-na-i-flory-ji-i-danihelka-mok-ady-/`]
-  },
-
-];
-
-export const mediaEn = [];
-
-export const popularizationCz = [
- {
-    date: `3/12/2019`,
-    name: `Botanici připravili nový klíč k určování rostlin. Minulý rychle zastaral`,
-    resources: [`Magazín M`],
-    links: [`https://www.em.muni.cz/veda-a-vyzkum/12300-botanici-pripravili-novy-klic-k-urcovani-rostlin?utm_source=fb_em&utm_medium=link&utm_campaign=facebook&fbclid=IwAR1--TWGjN1AddSnfvrM0w0geju_Yv_mkdOGzkKbLuhH3uMSn2yECESz45A`]
-  },
-  {
-    date: `1/4/2018`,
-    name: `Každý druh má v ekosystému svou roli`,
-    resources: [`Online.muni.cz`],
-    links: [`https://www.em.muni.cz/veda-a-vyzkum/10378-kazdy-druh-ma-v-ekosystemu-svou-roli`]
-  },
-  {
-    date: `11/3/2018`,
-    name: `Evropské druhy rostlin obsadily svět díky soužití s člověkem`,
-    resources: [`MUNI`],
-    links: [`https://www.em.muni.cz/veda-a-vyzkum/10308-evropske-druhy-rostlin-obsadily-svet-diky-souziti-s-clovekem`]
-  },
-  {
     date: `19/1/2017`,
     name: `První evropský červený seznam biotopů`,
     resources: [`Online.muni.cz`, `Ekolist.cz`, `Týden.cz`, `Novinky.cz (20/1)`, `Lidovky.cz (29/1)`, `Brněnský Deník (8/2)`],
     links: [`https://www.online.muni.cz/veda-a-vyzkum/8740-desitky-typu-evropske-prirody-jsou-v-ohrozeni-biologove-sestavili-cerveny-seznam`, `http://ekolist.cz/cz/zpravodajstvi/zpravy/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci`, `http://www.tyden.cz/rubriky/veda/priroda/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci_414283.html`, `https://www.novinky.cz/veda-skoly/427020-z-evropy-mizi-urcite-typy-prirodnich-stanovist.html`, `http://relax.lidovky.cz/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci-pxe-/veda.aspx?c=A170123_163447_ln_veda_ape`, `http://botzool.sci.muni.cz/news/2017_02_08_chytry_Brnensky_denik.jpg`]
   },
-    {
+  {
     date: `6/12/2016`,
     name: `První úplný přehled typů evropské vegetace`,
     resources: [`Online.muni.cz`, `Novinky.cz (7/12)`],
@@ -113,7 +102,218 @@ export const popularizationCz = [
     resources: [`Magazín M`],
     links: [`https://www.em.muni.cz/veda-a-vyzkum/3001-invaze-cizokrajnych-rostlin-v-hledacku-botaniku#.VBhS3fl_sn4`]
   },
-   {
+  {
+    date: `3/1/2013`,
+    name: `Aktuální soupis naší flóry (Jiří Danihelka)`,
+    resources: [`Český rozhlas Leonardo`, `Natura`],
+    links: [`https://hledani.rozhlas.cz/iRadio/?query=danihelka&reader=&stanice%5B%5D=%C4%8CRo+Leonardo&porad%5B%5D=`, `https://podtail.nl/podcast/natura-1/aktualni-soupis-na-i-flory-ji-i-danihelka-mok-ady-/`]
+  },
+
+];
+
+export const mediaEn = [
+{
+    date: `3/12/2019`,
+    name: `Botanici připravili nový klíč k určování rostlin. Minulý rychle zastaral`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/12300-botanici-pripravili-novy-klic-k-urcovani-rostlin?utm_source=fb_em&utm_medium=link&utm_campaign=facebook&fbclid=IwAR1--TWGjN1AddSnfvrM0w0geju_Yv_mkdOGzkKbLuhH3uMSn2yECESz45A`]
+  },
+  {
+    date: `22/8/2019`,
+    name: `Botanici zmapovali druhové bohatství evropských lesů`,
+    resources: [`ČT Události v regionech`,`ČT24`, `Magazín M`],
+    links: [`https://www.ceskatelevize.cz/porady/10122427178-udalosti-v-regionech-brno/319281381990819-udalosti-v-regionech/video/714129`, `https://ct24.ceskatelevize.cz/regiony/jihomoravsky-kraj/2900171-brnensti-vedci-mapuji-rozmanitost-evropskych-lesu-cesko-je`, `https://www.em.muni.cz/veda-a-vyzkum/11988-botanici-zmapovali-druhove-bohatstvi-evropskych-lesu`]
+  },
+{
+    date: `12/7/2019`,
+    name: `Sekání trávy ve městech`,
+    resources: [`ČT Události v regionech`],
+    links: [`https://www.ceskatelevize.cz/porady/10122427178-udalosti-v-regionech-brno/319281381990712-udalosti-v-regionech/video/707729`]
+  },
+  {
+    date: `7/11/2018`,
+    name: `Brněnští vědci odhalili tajemství úspěchu invazivních rostlin`,
+    resources: [`Pozitivní zprávy`,`ČT24`, `ScienceMag`, `Online.muni.cz`],
+    links: [`https://pozitivni-zpravy.cz/cesti-vedci-rozklicovali-tajemstvi-uspechu-invazivnich-rostlin/`,`https://ct24.ceskatelevize.cz/veda/2644739-brnensti-vedci-odhalili-tajemstvi-uspechu-invazivnich-rostlin-hlavni-roli-podle-nich`, `https://sciencemag.cz/invazni-rostliny-jsou-v-prumeru-vyssi-nez-puvodni-druhy/`, `https://www.online.muni.cz/veda-a-vyzkum/11178-objev-invazni-rostliny-jsou-v-prumeru-vyssi-nez-puvodni-druhy`]
+  },
+  {
+    date: `1/4/2018`,
+    name: `Každý druh má v ekosystému svou roli`,
+    resources: [`Online.muni.cz`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/10378-kazdy-druh-ma-v-ekosystemu-svou-roli`]
+  },
+  {
+    date: `24/3/2018`,
+    name: `Česká flóra a vegetace na jednom místě`,
+    resources: [`Česká televize, Studio 6`],
+    links: [`https://www.ceskatelevize.cz/ivysilani/10441287766-studio-6-vikend/218411010120324/obsah/609284-ceska-flora-a-vegetace-na-jednom-miste`]
+  },
+  {
+    date: `20/3/2018`,
+    name: `Botanici spustili nejrozsáhlejší online databázi české flóry a vegetace`,
+    resources: [`Česká televize`, `České noviny`, `MUNI`, `Radio.cz`, `Sciencemag`, `Ekolist`, `Enviweb`, `21. století`, `Týden.cz`],
+    links: [`http://www.ceskatelevize.cz/ct24/veda/2424340-cela-kvetena-na-jednom-miste-botanici-spustili-nejrozsahlejsi-online-databazi-ceske`,
+	`http://www.ceskenoviny.cz/zpravy/webova-databaze-shrnuje-informace-o-rostlinnych-druzich-v-cesku/1599651`,
+	`https://www.online.muni.cz/veda-a-vyzkum/10368-botanici-spustili-nejrozsahlejsi-online-databazi-ceske-flory-a-vegetace`,
+	`http://www.radio.cz/ru/rubrika/radiogazeta/vsya-flora-chexii-na-odnom-sajte`,
+	`http://sciencemag.cz/nejrozsahlejsi-online-databaze-ceske-flory-a-vegetace/`, `https://ekolist.cz/cz/zpravodajstvi/zpravy/ode-dneska-je-pristupna-nejrozsahlejsi-online-databaze-ceske-flory-a-vegetace`,
+	`http://www.enviweb.cz/rss/164353`, `https://21stoleti.cz/2018/03/21/veskere-informace-o-ceske-kvetene-na-jednom-miste/`, `https://www.tyden.cz/rubriky/veda/priroda/webova-databaze-shrnuje-informace-o-flore-a-vegetaci-v-cesku_472193.html`, ]
+  },
+  {
+    date: `11/3/2018`,
+    name: `Evropské druhy rostlin obsadily svět díky soužití s člověkem`,
+    resources: [`MUNI`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/10308-evropske-druhy-rostlin-obsadily-svet-diky-souziti-s-clovekem`]
+  },
+  
+    {
+    date: `9/1/2018`,
+    name: `Evropské rostliny ve světě`,
+    resources: [`Česká televize (1:11:00)`],
+    links: [`https://www.ceskatelevize.cz/ivysilani/10101491767-studio-ct24/218411058060109`]
+  },
+  {
+    date: `9/3/2017`,
+    name: `Červený seznam ohrožených ekosystémů`,
+    resources: [`Česká televize, Studio 6`],
+    links: [`https://www.ceskatelevize.cz/ivysilani/1096902795-studio-6/217411010100309/obsah/528996-seznam-ohrozenych-ekosystemu-v-evrope`]
+  },
+  {
+    date: `19/1/2017`,
+    name: `První evropský červený seznam biotopů`,
+    resources: [`Online.muni.cz`, `Ekolist.cz`, `Týden.cz`, `Novinky.cz (20/1)`, `Lidovky.cz (29/1)`, `Brněnský Deník (8/2)`],
+    links: [`https://www.online.muni.cz/veda-a-vyzkum/8740-desitky-typu-evropske-prirody-jsou-v-ohrozeni-biologove-sestavili-cerveny-seznam`, `http://ekolist.cz/cz/zpravodajstvi/zpravy/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci`, `http://www.tyden.cz/rubriky/veda/priroda/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci_414283.html`, `https://www.novinky.cz/veda-skoly/427020-z-evropy-mizi-urcite-typy-prirodnich-stanovist.html`, `http://relax.lidovky.cz/z-evropy-mizi-nektere-typy-prirodnich-stanovist-zjistili-vedci-pxe-/veda.aspx?c=A170123_163447_ln_veda_ape`, `http://botzool.sci.muni.cz/news/2017_02_08_chytry_Brnensky_denik.jpg`]
+  },
+  {
+    date: `6/12/2016`,
+    name: `První úplný přehled typů evropské vegetace`,
+    resources: [`Online.muni.cz`, `Novinky.cz (7/12)`],
+    links: [`https://www.online.muni.cz/veda-a-vyzkum/8576-botanici-muni-pomohli-s-popisem-evropske-vegetace-pomuze-chranit-prirodu?utm_source=fb_milujeme&utm_medium=link&utm_campaign=facebook`, `https://www.novinky.cz/veda-skoly/422968-brnensti-botanici-prispeli-k-popisu-evropske-vegetace-ktery-pomuze-chranit-prirodu.html`]
+  },
+  {
+    date: `13/7/2014`,
+    name: `Aplikace určí rostlinu na počkání`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/4532-aplikace-urci-rostlinu-na-pockani#.VBhSt_l_sn4`]
+  },
+  {
+    date: `11/3/2013`,
+    name: `Studentka vrací přírodu do lomu`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/student/3343-studentka-vraci-prirodu-do-lomu#.VBhUqPl_sn4`]
+  },
+  {
+    date: `26/11/2012`,
+    name: `Na Sibiři zkoumají českou dobu ledovou`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/3267-na-sibiri-zkoumaji-ceskou-dobu-ledovou#.VBhSzvl_sn4`]
+  },
+      {
+    date: `6/6/2012`,
+    name: `Invaze cizokrajných rostlin v hledáčku botaniků`,
+    resources: [`Magazín M`],
+    links: [`https://www.em.muni.cz/veda-a-vyzkum/3001-invaze-cizokrajnych-rostlin-v-hledacku-botaniku#.VBhS3fl_sn4`]
+  },
+  {
+    date: `3/1/2013`,
+    name: `Aktuální soupis naší flóry (Jiří Danihelka)`,
+    resources: [`Český rozhlas Leonardo`, `Natura`],
+    links: [`https://hledani.rozhlas.cz/iRadio/?query=danihelka&reader=&stanice%5B%5D=%C4%8CRo+Leonardo&porad%5B%5D=`, `https://podtail.nl/podcast/natura-1/aktualni-soupis-na-i-flory-ji-i-danihelka-mok-ady-/`]
+  },
+  ];
+
+export const popularizationCz = [
+{
+    date: `2019`,
+    name: `Biogeografie druhové diverzity rostlinných společenstev`,
+    resources: [`Živa`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Chytry_etal2019_Ziva.pdf`]
+  },
+  {
+    date: `2019`,
+    name: `Podpěrovské inspirace v botanice`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/podperovske-inspirace-v-botanice.pdf`]
+  },
+{
+    date: `2019`,
+    name: `Stepi západní Ukrajiny`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/2019-4/stepi-zapadni-ukrajiny.html`]
+  },
+  {
+    date: `2018`,
+    name: `Tvář jihomoravského luhu se mění`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/tvar-jihomoravskeho-luhu-se-meni.pdf`]
+  },
+  {
+    date: `2018`,
+    name: `Evropský červený seznam biotopů`,
+    resources: [`Ochrana přírody`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Chytry2018_Ochr-Prir.pdf`]
+  },
+  {
+    date: `2018`,
+    name: `Biotopy jako zdroje i příjemci nepůvodních druhů rostlin`,
+    resources: [`Živa`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Hejda_etal2018_Ziva.pdf`]
+  },
+{
+    date: `2018`,
+    name: `Známe odpověď na Darwinovu naturalizační hádanku? `,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/zname-odpoved-na-darwinovu-naturalizacni-hadanku.pdf`]
+  },
+  {
+    date: `2018`,
+    name: `Známe odpověď na Darwinovu naturalizační hádanku?`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/zname-odpoved-na-darwinovu-naturalizacni-hadanku.pdf`]
+  },
+  {
+    date: `2016`,
+    name: `Vliv akumulace dusíku na vřesoviště a suché trávníky v Národním parku Podyjí`,
+    resources: [`Životné prostredie`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Zahora_etal2016_Zivotne-prostredie.pdf`]
+  },
+    {
+    date: `2015`,
+    name: `Květena Brna – současný stav poznání`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/kvetena-brna-soucasny-stav-poznani.pdf`]
+  },
+  {
+    date: `2014`,
+    name: `Proč je flóra velkoměst pestrá`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/proc-je-flora-velkomest-pestra.pdf`]
+  },
+  {
+    date: `2013`,
+    name: `Botanické součty, rozdíly a podíly`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/botanicke-soucty-rozdily-a-podily.pdf`]
+  },
+ {
+    date: `2013`,
+    name: `Botanická fotogalerie a další pomůcky k určování rostlin`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/botanicka-fotogalerie-a-dalsi-pomucky-k-urcovani-r.pdf`]
+  },
+{
+    date: `2012`,
+    name: `GIS a jeho využití v botanice`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/gis-a-jeho-vyuziti-v-botanice.pdf`]
+  },
+  {
+    date: `2012`,
+    name: `Lesostep Jižního Uralu`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/lesostep-jizniho-uralu.pdf`]
+  },
+    {
     date: `2012`,
     name: `Ekosystémy Sibiře: analogie zaniklé přírody střední Evropy`,
     resources: [`Živa`],
@@ -125,11 +325,65 @@ export const popularizationCz = [
     resources: [`Živa`],
     links: [`http://ziva.avcr.cz/2012-4/rodinne-stribro-ceske-vegetace-mame-u-nas-neco-co-jinde-nemaji.html`]
   },
-   {
-    date: `9/2010`,
+  {
+    date: `2010`,
+    name: `Fenomén subkontinentálních doubrav: diverzita, dynamika a historie jednoho neobyčejného společenstva`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/fenomen-subkontinentalnich-doubrav-diverzita-dynam.pdf`]
+  },
+     {
+    date: `2010`,
+    name: `Co je to fytocenologický snímek`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/co-je-to-fytocenologicky-snimek.pdf`]
+  },
+     {
+    date: `2010`,
     name: `Kde dnes znamená včera. Jihosibiřské refugium doby ledové`,
     resources: [`Vesmír`],
     links: [`https://vesmir.cz/cz/casopis/archiv-casopisu/2010/cislo-9/kde-dnes-znamena-vcera.html`]
+  },
+  {
+    date: `2010`,
+    name: `Krajiny zamrzlé v čase I. Jižní Sibiř – současná analogie střední Evropy v době ledové`,
+    resources: [`Živa`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Horsak-Chytry2010ab_Ziva.pdf`]
+  },
+  {
+    date: `2010`,
+    name: `Krajiny zamrzlé v čase II. Jižní Ural – současná analogie střední Evropy ve starém a středním holocénu`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/krajiny-zamrzle-v-case-ii-jizni-ural-soucasna-anal.pdf`]
+  },
+  {
+    date: `2009`,
+    name: `Kam se šíří zavlečené rostliny? 1. Rozdíly v invadovanosti velkých území`,
+    resources: [`Živa`],
+    links: [`https://www.sci.muni.cz/botany/chytry/Chytry-Pysek2009abc_Ziva.pdf`]
+  },
+  {
+    date: `2009`,
+    name: `Kam se šíří zavlečené rostliny? 2. Invadovanost a invazibilita rostlinných společenstev`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/kam-se-siri-zavlecene-rostliny-2-invadovanost-a-in.pdf`]
+  },
+  {
+    date: `2009`,
+    name: `Kam se šíří zavlečené rostliny? 3. Obecné příčiny invazibility společenstev`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/kam-se-siri-zavlecene-rostliny-3-obecne-priciny-in.pdf`]
+  },
+  {
+    date: `2008`,
+    name: `„Taiwan? A mají tam vůbec nějaký les?“`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/taiwan-a-maji-tam-vubec-nejaky-les.pdf`]
+  },
+    {
+    date: `2005`,
+    name: `Sveřep stoklasa - starobylý plevel obilných polí`,
+    resources: [`Živa`],
+    links: [`http://ziva.avcr.cz/files/ziva/pdf/sverep-stoklasa-starobyly-plevel-obilnych-poli.pdf`]
   },
 ];
 
